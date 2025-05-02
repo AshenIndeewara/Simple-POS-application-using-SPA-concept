2e594fd4d00ccdd5d46c2851aedb360e8d9814d5
Ashen Indeewara <a.indeewara911@gmail.com>
2025-05-02 09:11:11 +0530

    feat: Implement customer, item, and order management functionality
    - Added CustomerController.js to handle customer operations including save, update, delete, and search.
    - Introduced ItemController.js for item management with similar functionalities.
    - Created OrderController.js to manage order placements and item selections.
    - Developed CustomerModel.js, ItemModel.js, and OrderModel.js for data handling and CRUD operations.
    - Established a simple in-memory database in Db.js for customers, items, and orders.
    - Enhanced index.html with structured sections for customer, item, and order management.
    - Implemented form validation and dynamic ID generation for customers and items.
    - Integrated jQuery for DOM manipulation and AJAX requests to simulate backend interactions.

---
+++ db/Db.js
@@ -0,0 +1,30 @@
+export let Customers = [
+    {
+        custId : 'C01',
+        custName : 'Ashen',
+        custAddress : 'Indeewara',
+        custSalary : '20'
+    } ,
+
+    {
+        custId : 'C02',
+        custName : 'Dasun',
+        custAddress : 'Lakshan',
+        custSalary : '40'
+    }
+];
+export let Items = [
+    {
+        itemId : 'I01',
+        itemName : 'Milk',
+        itemQty : 10,
+        itemPrice : 3
+    },
+    {
+        itemId : 'I02',
+        itemName : 'Water',
+        itemQty : 10,
+        itemPrice : 3
+    }
+];
+export let Orders = [];
